85a1eb3f7c0bf170690bcdf6af405d18f32317d3
Narotsit-Karki <narotsitkarki1234@gmail.com>
2022-12-16 17:01:46 +0545

    Font awesome package added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,14 +9,14 @@ export const Home = () =>
         <div className="row">
             <div className="col-auto">
                 <Link to="/" className="link-success text-decoration-none">
-                    <i class="fa-solid fa-film"></i><h1> Movie Info</h1>
+                    <h1><i class="fa-solid fa-film"></i> Movie Info</h1>
                 </Link>
 
             </div>
             <div className="col-4 ms-auto">
                 <div className="input-group mt-2">
                     <input type="search" className="form-control" placeholder="Search  movie and more ..."></input>
-                    <button className="btn btn-success"><i className="fa-solid fa-search"></i>search</button>
+                    <button className="btn btn-success"><i className="fa-solid fa-search"></i></button>
                 </div>
             </div>
         </div>
